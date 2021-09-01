68bf3103670cf0dee63be5d1aec70878cb3bf14b
Muhammad Raffly <rafli7654@gmail.com>
2021-09-01 16:44:40 +0700

    health, anim hit obstacle, collider prefabs

--- Assets/Script/scriptMati.cs
+++ Assets/Script/scriptMati.cs
@@ -6,13 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class scriptMati : MonoBehaviour
 {
+    public int health ;
+    public Text healthUI;
     public GameObject mati;
+
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Obstacle"))
         {
-            Time.timeScale = 0;
-            mati.SetActive(true);
+            health -= 1;
+            healthUI.text = health.ToString();
+            GetComponent<Animator>().SetBool("isHit", true);
+            if (health == 0)
+            {
+                Time.timeScale = 0;
+                mati.SetActive(true);
+            }
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isHit", false);
         }
     }
 }
